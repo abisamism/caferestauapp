8c764e3c17e9a8e4bbaa9e9d222049553e4dc113
Aashish Bhandari <abisamism@gmail.com>
2020-05-04 17:28:00 +0545

    Minor Changes

--- smartbuilder/settings.py
+++ smartbuilder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(qd+i65f4ss+axl9vw@v8d^!p!4=j3snia_xz48g3&p5(&tovp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','192.168.1.65','127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'smartbuilder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
